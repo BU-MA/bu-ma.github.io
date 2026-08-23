024b40d9126add19db7a2877f3a1b3d65bc0ea99
Hyperion <83.talbert@gmail.com>
2026-08-23 17:48:08 -0400

    added events, added all the attendance qr codes, deleted /src/css-redesign-showcase

--- README.md
+++ README.md
@@ -3,57 +3,18 @@ The official website of the Boston University Mathematics Association.
 
 This website was developed by Grant Talbert.
 
-## ignore this
+## final todo stuff
 
-brainstorming some ideas for stuff to add
-- PICTURES OF PAST EVENTSSSSS
-- problem of the week (putnam style)
-  - should have a way for ppl to submit this, might just go with a google form cuz idk how to protect against code injections
-  - possible prize for the winner? dunno if we can afford that. maybe a prize for whoever gets the most right over the sem
-- reu/course resources page
-- we should have an integration bee leaderboard that also shows past events
-  - we could also archive problems
+- totw script
+- ppotw & ppotw script (do we wanna do ppotw lowkey)
+- ppotw accessibility + mobile friendlyness
+- set up ttotw & ppotw at some point
+- light mode & funny notif
+- hook in attendance script
+- ensure metadata
+
+
+## longterm notes
+- pictures
 - alumni page
-- add metadata to more nicely format discord links
-- maybe each event gets its own page?
-- possibly refactor talks into "posts" which contains lecture notes + posts about like the theorem of the week for example and solns to the problem of the week and newsletters we might make etc
-- mobile + accessibility + low motion
-  - the mobile nav menus and homepage title card are FUCKED
-  - need to pass over the css and ensure accessibility features and people who prefer low motion get what they want
-- add a light mode for each theme and remove the default theme
-  - maybe add notifications for when you change themes so if ppl change to light mode i can say "Why...?"
-
-## some todo stuff
-
-- the past semester events pages are not good. need to do some work on past-events.js to get that running correctly
-- css for (talks -> posts)
-- css for contacts
-- silly page
-- alum page
-- css for past sem events pages
-- set up theorems of the week
-- set up weekly putnam problem
-- light mode & notifs
-- set a default theme
-- mobile + accessibility considerations
-- edit content of leadership page
-- possibly edit content of about page
-
-
-
-
-example code that can go into a `post-detail.css`-themed post
-```html
-    <h2>On the Schedule</h2>
-    <p>General meetings and Putnam practice both continue on their usual nights:</p>
-    <div class="post-schedule">
-      <div class="schedule-row">
-        <span class="schedule-day">Wed</span>
-        <span class="schedule-detail">General meetings &middot; CDS 365, 6&ndash;7pm</span>
-      </div>
-      <div class="schedule-row">
-        <span class="schedule-day">Mon</span>
-        <span class="schedule-detail">Putnam practice with Prof. Weinstein &middot; CDS 365, 5:30&ndash;7:30pm</span>
-      </div>
-    </div>
-```
+- maybe a course info page
--- src/misc-js/attendance-qr-code-generator.js
+++ src/misc-js/attendance-qr-code-generator.js
@@ -21,7 +21,33 @@
         [
             // add your times and links here!
             // be sure to put commas after all of them, except the last one
-            ["2026-09-22T18:00:00", "https://forms.gle/7yhTPbNwTEmpKLCJ6"]
+            ["2026-08-22T18:00:00", "https://forms.gle/7yhTPbNwTEmpKLCJ6"], // interest form so we could show this at splash if needed
+            ["2026-09-09T17:00:00", "https://forms.gle/7U9XUwx8FHFcRiyr7"],
+            ["2026-09-14T17:30:00", "https://forms.gle/3pL4swq8Q5Swp2mc9"],
+            ["2026-09-16T17:00:00", "https://forms.gle/UJkGDPAfazGJBEYF8"],
+            ["2026-09-21T17:30:00", "https://forms.gle/vfMWvqWu8qRhD3yS9"],
+            ["2026-09-23T17:00:00", "https://forms.gle/ATMLvAVfdqHjZXPK9"],
+            ["2026-09-28T17:30:00", "https://forms.gle/jPFc9m8Nx4sGJ5XCA"],
+            ["2026-09-30T17:00:00", "https://forms.gle/3qiS6zu584qrCU4N6"],
+            ["2026-10-05T17:30:00", "https://forms.gle/ykoaqEhceZRKSuZN7"],
+            ["2026-10-07T17:00:00", "https://forms.gle/ktJtQjB8Ui5tCA5n7"],
+            ["2026-10-12T17:30:00", "https://forms.gle/93r4c4Ut76vjxLVTA"],
+            ["2026-10-14T17:00:00", "https://forms.gle/kUCAns3RycsVrfYC7"],
+            ["2026-10-19T17:30:00", "https://forms.gle/wLf2EsqjiKJMQDWx6"],
+            ["2026-10-21T17:00:00", "https://forms.gle/6taFevu1GAaDCQNMA"],
+            ["2026-10-26T17:30:00", "https://forms.gle/WYrSQ46WUFHX6KoJ8"],
+            ["2026-10-28T17:00:00", "https://forms.gle/y99MkdDGPy3rtDXm7"],
+            ["2026-11-02T17:30:00", "https://forms.gle/4sh7uVXbboDFn7aa8"],
+            ["2026-11-04T17:00:00", "https://forms.gle/2vvKD5tkfzKQdpci9"],
+            ["2026-11-09T17:30:00", "https://forms.gle/LhYh2MzkbnaPm1iv9"],
+            ["2026-11-11T17:00:00", "https://forms.gle/HPEkVLyLPskXYnNr5"],
+            ["2026-11-16T17:30:00", "https://forms.gle/bbG8mxkyRKKQi87E8"],
+            ["2026-11-18T17:00:00", "https://forms.gle/XmBeozRksW3eDKdV6"],
+            ["2026-11-23T17:30:00", "https://forms.gle/o3F9c8nT2P9uNJiDA"],
+            ["2026-11-30T17:30:00", "https://forms.gle/JFyTG9n9AdyVmCB2A"],
+            ["2026-12-02T17:00:00", "https://forms.gle/EhEXPhad4rEMiJC39"],
+            ["2026-12-07T17:30:00", "https://forms.gle/b9pgj2zs3HPq8owL6"],
+            ["2026-12-09T17:00:00", "https://forms.gle/tLXwxTLXXEtgibAR9"]
         ].map(([d, l]) => [new Date(d), l])
     );
 
@@ -36,7 +62,7 @@
     // finds the first entry in dateToLink whose date is greater or equal to the current time
     const targetDate = dateToLink.keys().toArray()
         .sort((a, b) => a.getTime() - b.getTime())
-        .find(date => (date.getTime() - PREFIRE_TIME) >= now.getTime())
+        .find(date => (date.getTime() - PREFIRE_TIME) <= now.getTime())
 
     if (!targetDate) { return; }
 
